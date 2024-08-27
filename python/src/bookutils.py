b0d2b81f8b1af75ceea72b9ed970da0dee0b737b
Sree Sai Nandini Gundraju <gundrajusreesainandini@gmail.com>
2024-08-27 14:13:39 +0530

    r7

--- python/src/bookutils.py
+++ python/src/bookutils.py
@@ -1,3 +1,20 @@
+"""
+BookUtils Module
+
+This module provides utility functions to manage a library system, including book management, tracking the issue and return of books, and updating book statuses.
+
+
+Functions:
+    append_dict_to_csv(file_path,my_dict): Appends the dictionary my_dict as a new row into the file path
+    get_books(file_path): Reads all the rows from the file
+    find_book(file_path,name): Searches in the file by name
+    update_book_status(file_path,name,status): Updates the issued column of the name with the status
+
+Modules:
+    csv: Python library for handling csv files
+    numpy: Python library for data analysis
+    pandas: Python library for data visualisation
+"""
 import csv
 import numpy as np
 import pandas as pd
